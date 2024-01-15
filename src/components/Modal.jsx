9a16aa2fcab9aa04a6054d9ebdda63e7f33d9cdb
Sevdi Palazliev <palazliev@gmail.com>
2024-01-15 14:41:11 +0200

    Initial commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,34 @@
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = forwardRef(function Modal({ open, onClose, children }, ref) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+      close: () => {
+        dialog.current.close();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children : null}
+    </dialog>,
+    document.getElementById("modal")
+  );
+});
+
+export default Modal;
